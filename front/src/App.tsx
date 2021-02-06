501de22f8db7ff3f61fc08fc7ba17bbeef60d1ea
Seokseong Jeon <sjeon87@gmail.com>
2021-02-06 19:48:50 +0900

    Add frontend skeleton

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Menu } from 'semantic-ui-react';
+import Router from './Router';
 
 function App() {
   return (
     <div className="App">
-      <Menu>
-        <Menu.Item header>randevu</Menu.Item>
-        <Menu.Item>Features</Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item>Join</Menu.Item>
-          <Menu.Item>Login</Menu.Item>
-          <Menu.Item>Logout</Menu.Item>
-        </Menu.Menu>
-      </Menu>
-      <Container>
-        randevu
-      </Container>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front/src/Router.tsx
@@ -0,0 +1,26 @@
+import { Route, Switch, useHistory } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+export function Router() {
+  const history = useHistory();
+  return (
+    <>
+      <Menu>
+        <Menu.Item header onClick={() => history.push('/')}>randevu</Menu.Item>
+        <Menu.Item onClick={() => {history.push('/features')}}>Features</Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item onClick={() => history.push('/join')}>Join</Menu.Item>
+          <Menu.Item onClick={() => history.push('/login')}>Login</Menu.Item>
+          <Menu.Item>Logout</Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      <Switch>
+        <Route exact path='/'>randevu</Route>
+        <Route path='/features'>features</Route>
+        <Route path='*'>Not found</Route>
+      </Switch>
+    </>
+  );
+}
+
+export default Router;
