360ffd79a7e38157ceffa64aa3488759fb5ae445
Seokseong Jeon <sjeon87@gmail.com>
2021-02-07 13:04:09 +0900

    Convert Router to class (withRouter)

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,30 +1,35 @@
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Component } from "react";
+import { Route, RouteComponentProps, Switch, withRouter } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import Join from "./routes/join";
 import Login from "./routes/login";
 
-export function Router() {
-  const history = useHistory();
-  return (
-    <>
-      <Menu>
-        <Menu.Item header onClick={() => history.push('/')}>randevu</Menu.Item>
-        <Menu.Item onClick={() => {history.push('/features')}}>Features</Menu.Item>
-        <Menu.Menu position='right'>
-          <Menu.Item onClick={() => history.push('/join')}>Join</Menu.Item>
-          <Menu.Item onClick={() => history.push('/login')}>Login</Menu.Item>
-          <Menu.Item>Logout</Menu.Item>
-        </Menu.Menu>
-      </Menu>
-      <Switch>
-        <Route exact path='/'>randevu</Route>
-        <Route path='/features'>features</Route>
-        <Route path='/join'><Join /></Route>
-        <Route path='/login'><Login /></Route>
-        <Route path='*'>Not found</Route>
-      </Switch>
-    </>
-  );
+class Router extends Component<RouteComponentProps> {
+  render() {
+    const { history } = this.props;
+    return (
+      <>
+        <Menu>
+          <Menu.Item header onClick={() => history.push('/')}>randevu</Menu.Item>
+          <Menu.Item onClick={() => {history.push('/features')}}>Features</Menu.Item>
+          <Menu.Menu position='right'>
+            <Menu.Item onClick={() => history.push('/join')}>Join</Menu.Item>
+            <Menu.Item onClick={() => history.push('/login')}>Login</Menu.Item>
+            <Menu.Item>Logout</Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <Switch>
+          <Route exact path='/'>randevu</Route>
+          <Route path='/features'>features</Route>
+          <Route path='/join'><Join /></Route>
+          <Route path='/login'><Login /></Route>
+          <Route path='*'>Not found</Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
-export default Router;
+const RouterWithRouter = withRouter(Router);
+
+export default RouterWithRouter;
