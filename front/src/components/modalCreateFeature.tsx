432dd42294c09c6a4e468e4de2aab85b68a13639
Seokseong Jeon <sjeon87@gmail.com>
2021-02-09 01:00:10 +0900

    Working on modal creating a feature

--- front/src/components/modalCreateFeature.tsx
+++ front/src/components/modalCreateFeature.tsx
@@ -1,12 +1,16 @@
+import axios from "axios";
+import { config } from "randevu-shared/dist/config";
 import { Component } from "react";
-import { Button, Form, Modal, ModalProps } from "semantic-ui-react";
+import { Button, Form, Icon, Label, Modal, ModalProps } from "semantic-ui-react";
 
 type Props = {} & ModalProps;
 
 type State = {
   featureId: string,
   featureName: string,
-  owner: string,
+  username: string,
+  userId: string,
+  usernameFound: string,
 };
 
 class ModalCreateFeature extends Component<Props, State> {
@@ -15,11 +19,21 @@ class ModalCreateFeature extends Component<Props, State> {
     this.state = {
       featureId: '',
       featureName: '',
-      owner: '',
+      username: '',
+      userId: '',
+      usernameFound: '',
     }
     this.onChangeFeatureId = this.onChangeFeatureId.bind(this);
     this.onChangeFeatureName = this.onChangeFeatureName.bind(this);
     this.onChangeOwner = this.onChangeOwner.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.removeOwner = this.removeOwner.bind(this);
+    const { api } = config;
+    axios.defaults.baseURL = `http://${api.host}:${api.port}`;
+    axios.defaults.withCredentials = true;
+  }
+
+  createFeature() {
   }
 
   onChangeFeatureId(e: React.ChangeEvent<HTMLInputElement>) {
@@ -33,14 +47,31 @@ class ModalCreateFeature extends Component<Props, State> {
   }
 
   onChangeOwner(e: React.ChangeEvent<HTMLInputElement>) {
-    const owner = e.target.value;
-    this.setState({ owner });
+    const username = e.target.value;
+    this.setState({ username });
+  }
+
+  onKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.code !== 'Enter') {
+      return;
+    }
+    const { username } = this.state;
+    axios.get(`/users/${username}`).then((value) => {
+      const { userId, username: usernameFound } = value.data;
+      this.setState({ userId, usernameFound });
+    }).catch((reason) => {
+      console.error(reason);
+    });
+  }
+
+  removeOwner() {
+    this.setState({ userId: '', usernameFound: '' });
   }
 
   render() {
     const { closeAction, ...modalProps } = this.props;
-    const { featureId, featureName, owner } = this.state;
-    const disabled = !featureId || !featureName || !owner;
+    const { featureId, featureName, username, usernameFound } = this.state;
+    const disabled = !featureId || !featureName || !usernameFound;
     return (
       <Modal {...modalProps} onClose={closeAction}>
         <Modal.Header>Create a feature</Modal.Header>
@@ -54,15 +85,23 @@ class ModalCreateFeature extends Component<Props, State> {
               <label>Feature name</label>
               <input type='text' value={featureName} onChange={this.onChangeFeatureName} />
             </Form.Field>
-            <Form.Field error={!owner}>
+            <Form.Field error={!usernameFound}>
               <label>Owner</label>
-              <input type='text' value={owner} onChange={this.onChangeOwner} />
+              <input type='text' value={username} onChange={this.onChangeOwner} onKeyUp={this.onKeyUp} />
             </Form.Field>
           </Form>
+          {
+            usernameFound ? (
+              <Label onClick={this.removeOwner}>
+                {usernameFound}
+                <Icon name='delete' />
+              </Label>
+            ) : <></>
+          }
         </Modal.Content>
         <Modal.Actions>
-          <Button color='red' onClick={closeAction}>Cancel</Button>
-          <Button color='green' disabled={disabled}>Create</Button>
+          <Button onClick={closeAction}>Cancel</Button>
+          <Button color='green' disabled={disabled} onClick={this.createFeature}>Create</Button>
         </Modal.Actions>
       </Modal>
     );
