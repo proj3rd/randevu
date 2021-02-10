fe0237eefc5b7b38aad019591066356f87f73a7c
Seokseong Jeon <sjeon87@gmail.com>
2021-02-10 22:13:07 +0900

    Add more collections

--- back/src/constants.ts
+++ back/src/constants.ts
@@ -1,8 +1,14 @@
 export const COLLECTION_CUSTOMER = 'customer';
+export const COLLECTION_DEPLOYMENT_OPTION = 'deploymentOption';
 export const COLLECTION_FEATURE = 'feature';
 export const COLLECTION_FEATURE_VERSION = 'featureVersion';
+export const COLLECTION_NETWORK_ENTITY = 'networkEntity';
+export const COLLECTION_NETWORK_FUNCTION = 'networkFunction';
+export const COLLECTION_OPERATOR = 'operator';
 export const COLLECTION_PACKAGE_MAIN = 'packageMain';
 export const COLLECTION_PACKAGE_SUB = 'packageSub';
+export const COLLECTION_RADIO_ACCESS_TECH = 'radioAccessTech';
+export const COLLECTION_RAN_SHARING = 'ranSharing';
 export const COLLECTION_USER = 'user';
 
 export const EDGE_COLLECTION_COMMITS = 'commits';
