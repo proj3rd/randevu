8ec57f09048dffc995a1513f0768e115fd9776aa
Seokseong Jeon <sjeon87@gmail.com>
2021-03-14 01:46:53 +0900

    Support creating a main/sub package

--- back/src/services/package.ts
+++ back/src/services/package.ts
@@ -1,7 +1,7 @@
 import { Database } from "arangojs";
 import { Transaction } from "arangojs/transaction";
 import { Express } from 'express';
-import { COLLECTION_OPERATOR, COLLECTION_PACKAGE_MAIN, COLLECTION_PACKAGE_SUB, COLLECTION_USER, EDGE_COLLECTION_OWNS, EDGE_COLLECTION_SUCCEEDS, EDGE_COLLECTION_TARGETS } from "../constants";
+import { COLLECTION_OPERATOR, COLLECTION_PACKAGE_MAIN, COLLECTION_PACKAGE_SUB, COLLECTION_USER, EDGE_COLLECTION_DERIVED_FROM, EDGE_COLLECTION_OWNS, EDGE_COLLECTION_SUCCEEDS, EDGE_COLLECTION_TARGETS } from "../constants";
 import { User } from "randevu-shared/dist/types";
 import { validateString } from "../utils";
 
@@ -105,6 +105,7 @@ export function servicePackage(app: Express, db: Database) {
     }
     let trx: Transaction | undefined;
     try {
+      const collectionDerivedFrom = db.collection(EDGE_COLLECTION_DERIVED_FROM);
       const collectionOperator = db.collection(COLLECTION_OPERATOR);
       const collectionOwns = db.collection(EDGE_COLLECTION_OWNS);
       const collectionPackageMain = db.collection(COLLECTION_PACKAGE_MAIN);
@@ -114,96 +115,73 @@ export function servicePackage(app: Express, db: Database) {
       const collectionUser = db.collection(COLLECTION_USER);
       trx = await db.beginTransaction({
         read: [collectionOperator, collectionUser],
-        write: [collectionPackageMain, collectionPackageSub, collectionOwns, collectionSucceeds, collectionTargets],
+        write: [collectionDerivedFrom, collectionPackageMain, collectionPackageSub, collectionOwns, collectionSucceeds, collectionTargets],
       });
-      return res.status(501).end();
-      // Create package document
-    //   const cursorPackageFound = await trx.step(() => db.query({
-    //     query: `
-    //       FOR pacakge IN @@collectionPackage
-    //         FILTER pacakge.packageName == @packageName
-    //         LIMIT 1
-    //         RETURN pacakge._id
-    //     `,
-    //     bindVars: {
-    //       '@collectionPackage': collectionPackage.name, packageName,
-    //     },
-    //   }));
-    //   const packageFound = await cursorPackageFound.all();
-    //   if (packageFound.length) {
-    //     await trx.abort();
-    //     return res.status(400).json({ reason: `Duplicate package name` });
-    //   }
-    //   const pkg = await trx.step(() => collectionPackage.save({ packageName }));
-    //   // User -owns-> package
-    //   const cursorUserDocIdFound = await trx.step(() => db.query({
-    //     query: `
-    //       FOR user in @@collectionUser
-    //         FILTER user.username == @owner
-    //         LIMIT 1
-    //         RETURN user._id
-    //     `,
-    //     bindVars: { '@collectionUser': collectionUser.name, owner },
-    //   }));
-    //   const userDocIdFound = await cursorUserDocIdFound.all();
-    //   if (!userDocIdFound.length) {
-    //     await trx.abort();
-    //     return res.status(400).json({ reason: 'User not found' });
-    //   }
-    //   const userDocId = userDocIdFound[0];
-    //   await trx.step(() => collectionOwns.save({
-    //     _from: userDocId,
-    //     _to: pkg._id,
-    //   }));
-    //   // Package -targets-> operator
-    //   const cursorOperatorIdFound = await trx.step(() => db.query({
-    //     query: `
-    //       FOR operator IN @@collectionOperator
-    //         FILTER operator.operatorName == @operatorName
-    //         LIMIT 1
-    //         RETURN operator._id
-    //     `,
-    //     bindVars: { '@collectionOperator': collectionOperator.name, operatorName },
-    //   }));
-    //   const operatorIdFound = await cursorOperatorIdFound.all();
-    //   if (!operatorIdFound.length) {
-    //     await trx.abort();
-    //     return res.status(400).json({ reason: 'Operator not found' });
-    //   }
-    //   const operatorId = operatorIdFound[0];
-    //   await trx.step(() => collectionTargets.save({
-    //     _from: pkg._id,
-    //     _to: operatorId,
-    //   }));
-    //   // Package -success-> (previous) package
-    //   if (previousPackageName) {
-    //     if (typeof previousPackageName === 'string') {
-    //       const cursorPreviousPkgIdFound = await trx.step(() => db.query({
-    //         query: `
-    //           FOR package IN @@collectionPackage
-    //             FILTER package.packageName == @previousPackageName
-    //             LIMIT 1
-    //             RETURN package._id
-    //         `,
-    //         bindVars: { '@collectionPackage': collectionPackage.name, previousPackageName },
-    //       }));
-    //       const previousPackageIdFound = await cursorPreviousPkgIdFound.all();
-    //       if (!previousPackageIdFound.length) {
-    //         await trx.abort();
-    //         return res.status(400).json({ reason: 'Previous package not found' });
-    //       }
-    //       const previousPackageId = previousPackageIdFound[0];
-    //       await trx.step(() => collectionSucceeds.save({
-    //         _from: pkg._id,
-    //         _to: previousPackageId,
-    //       }));
-    //     } else {
-    //       await trx.abort();
-    //       return res.status(400);
-    //     }
-    //   }
-    //   await trx.commit();
-    //   return res.status(200).end();
+      // Check duplicate package name
+      const packageFound = await findPackageByName(db, trx, name);
+      if (packageFound.length) {
+        await trx.abort();
+        return res.status(400).json({ reason: 'Duplicate package name '});
+      }
+      if (!sub) {
+        // Create a main package
+        await trx.step(() => collectionPackageMain.save({ name }));
+        await trx.commit();
+        return res.status(200).end();
+      } else {
+        const { main, operator, previous, owner } = sub;
+        if (!validateString(main) || !validateString(operator) || !validateString(owner)
+            || (previous && !validateString(previous))) {
+          await trx.abort();
+          return res.status(400).end();
+        }
+        // Create a sub package
+        const packageSub = await trx.step(() => collectionPackageSub.save({ name }));
+        // Sub -derivedFrom-> Main
+        const packageMainFound = await trx.step(() => collectionPackageMain.document(main));
+        if (!packageMainFound) {
+          await trx.abort();
+          return res.status(400).json({ reason: 'Main package not found' });
+        }
+        await trx.step(() => collectionDerivedFrom.save({
+          _from: packageSub._id,
+          _to: packageMainFound._id,
+        }));
+        // Sub -targets-> Operator
+        const operatorFound = await trx.step(() => collectionOperator.document(operator));
+        if (!operatorFound) {
+          await trx.abort();
+          return res.status(400).json({ reason: 'Operator not found' });
+        }
+        await trx.step(() => collectionTargets.save({
+          _from: packageSub._id,
+          _to: operatorFound._id,
+        }));
+        // User -owns-> Sub
+        const ownerFound = await trx.step(() => collectionUser.document(owner));
+        if (!ownerFound) {
+          await trx.abort();
+          return res.status(400).json({ reason: 'Owner not found' });
+        }
+        await trx.step(() => collectionOwns.save({
+          _from: ownerFound._id,
+          _to: packageSub._id,
+        }));
+        // Sub -succeeds-> Previous
+        if (previous) {
+          const previousFound = await trx.step(() => collectionPackageSub.document(previous));
+          if (!previousFound) {
+            await trx.abort();
+            return res.status(400).json({ reason: 'Previous package not found' });
+          }
+          await trx.step(() => collectionSucceeds.save({
+            _from: packageSub._id,
+            _to: previousFound._id,
+          }));
+        }
+        await trx.commit();
+        return res.status(200).end();
+      }
     } catch (e) {
       if (trx) {
         await trx.abort();
@@ -212,4 +190,30 @@ export function servicePackage(app: Express, db: Database) {
       return res.status(500).end();
     }
   });
+}
+
+async function findPackageByName(db: Database, trx: Transaction, name: string) {
+  const packageMain = await findPackageByNameInCollection(db, trx, name, COLLECTION_PACKAGE_MAIN);
+  if (packageMain) {
+    return packageMain;
+  }
+  const packageSub = await findPackageByNameInCollection(db, trx, name, COLLECTION_PACKAGE_SUB);
+  if (packageSub) {
+    return packageSub;
+  }
+  return undefined;
+}
+
+async function findPackageByNameInCollection(db: Database, trx: Transaction, name: string, collectionName: string) {
+  const cursorPackageFound = await trx.step(() => db.query({
+    query: `
+      FOR package IN @@collectionPackage
+        FILTER package.name == @name
+        LIMIT 1
+        RETURN package
+    `,
+    bindVars: { '@collectionPackage': collectionName, username: name },
+  }));
+  const packageFound = await cursorPackageFound.all();
+  return packageFound[0];
 }
