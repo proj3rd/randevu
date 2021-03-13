5142879fcfdc5659f37eabd82b699d801ba10b9c
Seokseong Jeon <sjeon87@gmail.com>
2021-03-14 03:33:44 +0900

    Add validateStringList()

--- back/src/utils.ts
+++ back/src/utils.ts
@@ -1,3 +1,7 @@
 export function validateString(input: any) {
   return typeof input === 'string' && input;
 }
+
+export function validateStringList(input: any) {
+  return (input instanceof Array) && (input.every((item) => validateString(item)));
+}
