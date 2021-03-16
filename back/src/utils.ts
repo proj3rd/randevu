af9a0545bf9cbfc67b16b8e7841adfa01fc1666d
Seokseong Jeon <sjeon87@gmail.com>
2021-03-16 23:31:06 +0900

    Add mergeObjectList() util function

--- back/src/utils.ts
+++ back/src/utils.ts
@@ -1,3 +1,20 @@
+/**
+ * Merge item of `objList2` into item of `objList1` with `key2` and `key1`, respectively.
+ * It mutates `objList1` and `objList2`.
+ */
+export function mergeObjectList(objList1: any[], objList2: any[], key1: string, key2: string = key1) {
+  for (let i = 0; i < objList1.length; i += 1) {
+    const obj1 = objList1[i];
+    const j = objList2.findIndex((obj2) => obj2[key2] === obj1[key1]);
+    if (j === -1) {
+      continue;
+    }
+    const obj2 = objList2[j];
+    delete obj2[key2];
+    Object.assign(obj1, obj2);
+  }
+}
+
 export function validateString(input: any) {
   return typeof input === 'string' && input;
 }
