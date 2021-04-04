d2b96966f0613a837e21b4439e609b6335f87c80
Seokseong Jeon <sjeon87@gmail.com>
2021-04-04 22:20:16 +0900

    Add TC for adding a duplicate sub package

--- back/src/tests/package.ts
+++ back/src/tests/package.ts
@@ -144,3 +144,54 @@ it('Should pass adding a sub package', function(done) {
     done(reason);
   });
 });
+
+
+it('Should fail adding a duplicate sub package', function(done) {
+  let pkg: any;
+  let operator: any;
+  let owner: any;
+
+  axios.get('/packages').then((value) => {
+    const { data: packageList } = value;
+    const packageFound = packageList.find((pkg: any) => pkg.name === 'Main package A');
+    if (!packageFound) {
+      throw new Error('Package not found');
+    }
+    pkg = packageFound;
+    return axios.get('/operators');
+  }).then((value) => {
+    const { data: operatorList } = value;
+    const operatorFound = operatorList.find((operator: any) => operator.name === 'Verizon Wireless');
+    if (!operatorFound) {
+      throw new Error('Operator not found');
+    }
+    operator = operatorFound;
+    return axios.get('/users');
+  }).then((value) => {
+    const { data: userList } = value;
+    const userFound = userList.find((user: any) => user.username === 'sjeon');
+    if (!userFound) {
+      throw new Error('User not found');
+    }
+    owner = userFound;
+    return axios.post('/packages', {
+      name: 'Sub package A',
+      sub: {
+        main: pkg._id,
+        operator: operator._id,
+        owner: owner._id,
+      },
+    });
+  }).then(() => {
+    done(new Error());
+  }).catch((reason) => {
+    if (reason && statusCode(reason.response) === 400
+        && reason.response.data && reason.response.data.reason
+        && reason.response.data.reason === 'Duplicate package name') {
+      done();
+    } else {
+      console.log(reason);
+      done(reason);
+    }
+  });
+});
