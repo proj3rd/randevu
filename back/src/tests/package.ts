2c66c628a6e7958b80970c8001e8e701b4ea2b8f
Seokseong Jeon <sjeon87@gmail.com>
2021-04-04 22:04:23 +0900

    Add TC for adding sub packages

--- back/src/tests/package.ts
+++ back/src/tests/package.ts
@@ -65,3 +65,88 @@ it('Should fail adding a sub package without main package', function(done) {
     }
   })
 });
+
+it('Should fail adding a sub package without operator', function(done) {
+  axios.post('/packages', {
+    name: 'Sub package A',
+    sub: {
+      main: 'Document ID of main package',
+      owner: 'Document ID of user',
+    },
+  }).then(() => {
+    done(new Error());
+  }).catch((reason) => {
+    if (reason && statusCode(reason.response) === 400) {
+      done();
+    } else {
+      done(reason);
+    }
+  })
+});
+
+it('Should fail adding a sub package without owner', function(done) {
+  axios.post('/packages', {
+    name: 'Sub package A',
+    sub: {
+      main: 'Document ID of main package',
+      operator: 'Document ID of operator',
+    },
+  }).then(() => {
+    done(new Error());
+  }).catch((reason) => {
+    if (reason && statusCode(reason.response) === 400) {
+      done();
+    } else {
+      done(reason);
+    }
+  })
+});
+
+it('Should pass adding a sub package', function(done) {
+  let pkg: any;
+  let operator: any;
+  let owner: any;
+
+  axios.get('/packages').then((value) => {
+    return value.data;
+  }).then((packageList) => {
+    const packageFound = packageList.find((pkg: any) => pkg.name === 'Main package A');
+    if (!packageFound) {
+      throw new Error('Package not found');
+    }
+    pkg = packageFound;
+  }).then(() => {
+    return axios.get('/operators');
+  }).then((value) => {
+    return value.data;
+  }).then((operatorList) => {
+    const operatorFound = operatorList.find((operator: any) => operator.name === 'Verizon Wireless');
+    if (!operatorFound) {
+      throw new Error('Operator not found');
+    }
+    operator = operatorFound;
+  }).then(() => {
+    return axios.get('/users');
+  }).then((value) => {
+    return value.data;
+  }).then((userList) => {
+    const userFound = userList.find((user: any) => user.username === 'sjeon');
+    if (!userFound) {
+      throw new Error('User not found');
+    }
+    owner = userFound;
+  }).then(() => {
+    return axios.post('/packages', {
+      name: 'Sub package A',
+      sub: {
+        main: pkg._id,
+        operator: operator._id,
+        owner: owner._id,
+      },
+    });
+  }).then(() => {
+    done();
+  }).catch((reason) => {
+    done(reason);
+  });
+});
