c9efd7688f0223518604cddf64182f0e5cb90123
Seokseong Jeon <sjeon87@gmail.com>
2021-04-11 19:58:17 +0900

    Support adding an enum

--- front/src/routes/EnumManager.tsx
+++ front/src/routes/EnumManager.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { config} from 'randevu-shared/dist/config';
 import { DocEnum, User } from "randevu-shared/dist/types";
 import { useEffect, useState } from "react";
-import { Button, Container, Dimmer, Form, Header, Loader, Table } from "semantic-ui-react";
+import { Button, Container, Dimmer, Form, Header, Loader, Message, Table } from "semantic-ui-react";
 
 type Props = {
   title: string;
@@ -18,7 +18,29 @@ axios.defaults.withCredentials = true;
 export default function EnumManager({ title, path, user, onLogout }: Props) {
   const [enumName, setEnumName] = useState('');
   const [waiting, setWaiting] = useState(false);
+  const [messageVisible, setMessageVisible] = useState(false);
+  const [messageContent, setMessageContent] = useState('');
   const [enumList, setEnumList] = useState<DocEnum[]>([]);
+
+  function addEnum() {
+    if (!enumName) {
+      return;
+    }
+    setWaiting(true);
+    axios.post(path, { name: enumName }).then((response) => {
+      return axios.get(path);
+    }).then((response) => {
+      const { data: enumList } = response;
+      setEnumList(enumList);
+      setMessageVisible(false);
+    }).catch((reason) => {
+      console.error(reason);
+      setMessageContent('Oops. Something went wrong. Please try again later');
+      setMessageVisible(true);
+    }).finally(() => {
+      setWaiting(false);
+    });
+  }
 
   useEffect(() => {
     setWaiting(true);
@@ -49,16 +71,21 @@ export default function EnumManager({ title, path, user, onLogout }: Props) {
           <Table.Body>
             {
               user && user.role === 'admin' ? (
-                <Table.Row>
+                <Table.Row verticalAlign='top'>
                   <Table.Cell>
-                    <Form>
+                    <Form id='addEnum' onSubmit={addEnum}>
                       <Form.Field>
                         <input value={enumName} onChange={(e) => setEnumName(e.target.value)} />
                       </Form.Field>
                     </Form>
+                    {
+                      messageVisible ? (
+                        <Message visible negative>{messageContent}</Message>
+                      ) : (<></>)
+                    }
                   </Table.Cell>
                   <Table.Cell>
-                    <Button icon='plus' disabled={!enumName} />
+                    <Button type='submit' icon='plus' disabled={!enumName} form='addEnum' />
                   </Table.Cell>
                 </Table.Row>
               ) : (<></>)
