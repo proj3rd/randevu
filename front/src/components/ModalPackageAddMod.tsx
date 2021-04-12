d01b92fdb621d0305dc8b21bc1d8cabf4785ce42
Seokseong Jeon <sjeon87@gmail.com>
2021-04-12 15:38:55 +0900

    Working on ModalPackageAddMod

--- front/src/components/ModalPackageAddMod.tsx
+++ front/src/components/ModalPackageAddMod.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { DocPackage } from "randevu-shared/dist/types";
+import { DocOperator, DocPackage, DocUser } from "randevu-shared/dist/types";
 import { useEffect, useState } from "react";
-import { Button, Dimmer, Divider, DropdownItemProps, Form, Loader, Modal, ModalProps, Select } from "semantic-ui-react";
+import { Button, Dimmer, Divider, DropdownItemProps, DropdownProps, Form, Loader, Modal, ModalProps, Select } from "semantic-ui-react";
+import EnumSelector from "./EnumSelector";
 
 type Props = {} & ModalProps;
 
@@ -10,11 +11,17 @@ export default function ModalPackageAddMod({ ...modalProps }: Props) {
 
   const [waiting, setWaiting] = useState(false);
   const [packageMainList, setPackageMainList] = useState<DropdownItemProps[]>([]);
+  const [operatorList, setOperatorList] = useState<DropdownItemProps[]>([]);
   const [userList, setUserList] = useState<DropdownItemProps[]>([]);
   const [packageSubList, setPackageSubList] = useState<DropdownItemProps[]>([]);
+  const [deploymentOptionList, setDeploymentOptionList] = useState<any[]>([]);
+  const [productList, setProductList] = useState<any[]>([]);
+  const [ratList, setRatList] = useState<any[]>([]);
+  const [ranSharingList, setRanSharingList] = useState<any[]>([]);
 
   const [name, setName] = useState('');
   const [packageMain, setPackageMain] = useState('');
+  const [operator, setOperator] = useState('');
   const [owner, setOwner] = useState('');
 
   useEffect(() => {
@@ -37,13 +44,80 @@ export default function ModalPackageAddMod({ ...modalProps }: Props) {
     });
   }, []);
 
+  function disabled() {
+    if (!name) {
+      return true
+    };
+    if (!packageMain) {
+      return false
+    };
+    if (!operator) {
+      return true
+    };
+    if (!owner) {
+      return true
+    };
+  }
+
+  function onChangePackageMain(event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) {
+    const packageMain = data.value as string;
+    setPackageMain(packageMain);
+    if (!packageMain) {
+      return;
+    }
+    setWaiting(true);
+    axios.get('/users').then((response) => {
+      const { data: userList } = response;
+      setUserList(userList);
+      return axios.get('/operators');
+    }).then((response) => {
+      const operatorList = response.data.map((item: DocOperator) => {
+        const { _id, name } = item;
+        return { key: _id, value: _id, text: name };
+      });
+      setOperatorList(operatorList);
+      return axios.get('/users');
+    }).then((response) => {
+      const userList = response.data.map((item: DocUser) => {
+        const { _id, username } = item;
+        return { key: _id, value: _id, text: username };
+      });
+      setUserList(userList);
+      return axios.get('/deployment-options');
+    }).then((response) => {
+      const { data: deploymentOptionList } = response;
+      setDeploymentOptionList(deploymentOptionList);
+      return axios.get('/products');
+    }).then((response) => {
+      const { data: productList } = response;
+      setProductList(productList);
+      return axios.get('/radio-access-technologies');
+    }).then((response) => {
+      const { data: ratList } = response;
+      setRatList(ratList);
+      return axios.get('/ran-sharing');
+    }).then((response) => {
+      const { data: ranSharingList } = response;
+      setRanSharingList(ranSharingList);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaiting(false);
+    });
+  }
+
+  function onChangeOperator(event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) {
+    setOperator(data.value as string);
+    // TODO
+  }
+
   return (
     <Modal {...modalProps}>
-      <Modal.Header></Modal.Header>
+      <Modal.Header>Add a package</Modal.Header>
       <Modal.Content>
         <Dimmer.Dimmable>
           <Form>
-            <Form.Field>
+            <Form.Field error={!name}>
               <label>Name</label>
               <input value={name} onChange={(e) => setName(e.target.value)} />
             </Form.Field>
@@ -52,12 +126,22 @@ export default function ModalPackageAddMod({ ...modalProps }: Props) {
               <label>Main package</label>
               <Select
                 search options={packageMainList}
-                value={packageMain} onChange={(e, d) => setPackageMain(d.value as string)}
+                value={packageMain} onChange={onChangePackageMain}
               />
             </Form.Field>
-            <Form.Field disabled={!packageMain}>
+            <Form.Field error={packageMain && !operator} disabled={!packageMain}>
+              <label>Operator</label>
+              <Select
+                search options={operatorList}
+                value={operator} onChange={onChangeOperator}
+              />
+            </Form.Field>
+            <Form.Field error={packageMain && !owner} disabled={!packageMain}>
               <label>Owner</label>
-              <Select select options={userList} />
+              <Select
+                search options={userList}
+                value={owner} onChange={(e, d) => setOwner(d.value as string)}
+              />
             </Form.Field>
             <Divider horizontal>Additional information</Divider>
             <Form.Field disabled={!packageMain}>
@@ -66,15 +150,19 @@ export default function ModalPackageAddMod({ ...modalProps }: Props) {
             </Form.Field>
             <Form.Field disabled={!packageMain}>
               <label>Deployment options</label>
+              <EnumSelector enumList={deploymentOptionList} />
             </Form.Field>
             <Form.Field disabled={!packageMain}>
               <label>Prodcuts</label>
+              <EnumSelector enumList={productList} />
             </Form.Field>
             <Form.Field disabled={!packageMain}>
               <label>Radio access technologies</label>
+              <EnumSelector enumList={ratList} />
             </Form.Field>
             <Form.Field disabled={!packageMain}>
               <label>RAN sharing</label>
+              <EnumSelector enumList={ranSharingList} />
             </Form.Field>
           </Form>
           <Dimmer active={waiting}>
@@ -86,7 +174,7 @@ export default function ModalPackageAddMod({ ...modalProps }: Props) {
         <Button onClick={(e, d) => onClose && onClose(e, {})}>Cancel</Button>
         <Button
           color='green'
-          disabled={!name || (!!packageMain && !owner)}
+          disabled={disabled()}
         >Add</Button>
       </Modal.Actions>
     </Modal>
