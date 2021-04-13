cc6b2770ff006fdfedae6eef48586fdb0bb4ca9e
Seokseong Jeon <sjeon87@gmail.com>
2021-04-14 00:11:21 +0900

    Working on getting information of a main/sub package

--- front/src/routes/packages/PackageInfo.tsx
+++ front/src/routes/packages/PackageInfo.tsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { DocUser } from "randevu-shared/dist/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Dimmer, Header, Loader } from "semantic-ui-react";
 
@@ -11,6 +12,17 @@ export default function PackageInfo({ user }: Props) {
   const { seqVal } = useParams() as any;
 
   const [waiting, setWaiting] = useState(false);
+
+  useEffect(() => {
+    setWaiting(true);
+    axios.get(`/packages/${seqVal}`).then((response) => {
+      console.log(response.data);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaiting(false);
+    })
+  }, [seqVal]);
 
   return (
     <Dimmer.Dimmable>
