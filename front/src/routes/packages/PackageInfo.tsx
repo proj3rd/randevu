19e048e37d8470f5f96d994287368fe578ffcc9f
Seokseong Jeon <sjeon87@gmail.com>
2021-04-14 23:59:32 +0900

    Working on view package information

--- front/src/routes/packages/PackageInfo.tsx
+++ front/src/routes/packages/PackageInfo.tsx
@@ -13,23 +13,25 @@ export default function PackageInfo({ user, type }: Props) {
   const { seqVal } = useParams() as any;
 
   const [waiting, setWaiting] = useState(false);
+  const [name, setName] = useState('');
 
   useEffect(() => {
     setWaiting(true);
-    axios.get(`/packages/${seqVal}`).then((response) => {
-      console.log(response.data);
+    axios.get(`/packages/${type}/${seqVal}`).then((response) => {
+      const { name } = response.data;
+      setName(name);
     }).catch((reason) => {
       console.error(reason);
     }).finally(() => {
       setWaiting(false);
     })
-  }, [seqVal]);
+  }, [type, seqVal]);
 
   return (
     <Dimmer.Dimmable>
       <Header as='h1'>
         <Header.Subheader>Packages</Header.Subheader>
-        {seqVal}
+        {name}
       </Header>
       <Dimmer active={waiting}>
         <Loader />
