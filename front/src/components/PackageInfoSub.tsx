b00d6a4e3ff6d6b8c75e8c8895cde3d8eebf3f37
Seokseong Jeon <sjeon87@gmail.com>
2021-04-19 22:12:11 +0900

    Render deployment options, products, RATs and RAN sharing requirements

--- front/src/components/PackageInfoSub.tsx
+++ front/src/components/PackageInfoSub.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { DocOperator, DocUser } from "randevu-shared/dist/types";
+import { DocEnum, DocOperator, DocUser } from "randevu-shared/dist/types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Dimmer, Header, Label, Loader } from "semantic-ui-react";
+import EnumList from "./EnumList";
 
 type Props = {
   user: DocUser | undefined;
@@ -15,6 +16,14 @@ export default function PackageInfoSub({ user }: Props) {
   const [waitingName, setWaitingName] = useState(false);
   const [operator, setOperator] = useState<DocOperator | undefined>(undefined);
   const [waitingOperator, setWaitingOperator] = useState(false);
+  const [deploymentOptionList, setDeploymentOptionList] = useState<DocEnum[]>([]);
+  const [waitingDeploymentOptionList, setWaitingDeploymentOptionList] = useState(false);
+  const [productList, setProductList] = useState<DocEnum[]>([]);
+  const [waitingProductList, setWaitingProductList] = useState(false);
+  const [ratList, setRatList] = useState<DocEnum[]>([]);
+  const [waitingRatList, setWaitingRatList] = useState(false);
+  const [ranSharingList, setRanSharingList] = useState<DocEnum[]>([]);
+  const [waitingRanSharingList, setWaitingRanSharingList] = useState(false);
 
   useEffect(() => {
     setWaitingName(true);
@@ -36,6 +45,46 @@ export default function PackageInfoSub({ user }: Props) {
     }).finally(() => {
       setWaitingOperator(false);
     });
+
+    setWaitingDeploymentOptionList(true);
+    axios.get(`/packages/sub/${seqVal}/deployment-options`).then((response) => {
+      const { data: deploymentOptionList } = response;
+      setDeploymentOptionList(deploymentOptionList);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaitingDeploymentOptionList(false);
+    });
+
+    setWaitingProductList(true);
+    axios.get(`/packages/sub/${seqVal}/products`).then((response) => {
+      const { data: productList } = response;
+      setProductList(productList);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaitingProductList(false);
+    });
+
+    setWaitingRatList(true);
+    axios.get(`/packages/sub/${seqVal}/radio-access-technologies`).then((response) => {
+      const { data: ratList } = response;
+      setRatList(ratList);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaitingRatList(false);
+    });
+
+    setWaitingRanSharingList(true);
+    axios.get(`/packages/sub/${seqVal}/ran-sharing`).then((response) => {
+      const { data: ranSharingList } = response;
+      setRanSharingList(ranSharingList);
+    }).catch((reason) => {
+      console.error(reason);
+    }).finally(() => {
+      setWaitingRanSharingList(false);
+    });
   }, [seqVal]);
 
   return (
@@ -56,9 +105,33 @@ export default function PackageInfoSub({ user }: Props) {
         </Dimmer>
       </Dimmer.Dimmable>
       <Header as='h2'>Deployment options</Header>
+      <Dimmer.Dimmable>
+        <EnumList enumList={deploymentOptionList} />
+        <Dimmer active={waitingDeploymentOptionList}>
+          <Loader />
+        </Dimmer>
+      </Dimmer.Dimmable>
       <Header as='h2'>Products</Header>
+      <Dimmer.Dimmable>
+        <EnumList enumList={productList} />
+        <Dimmer active={waitingProductList}>
+          <Loader />
+        </Dimmer>
+      </Dimmer.Dimmable>
       <Header as='h2'>Radio access technologies</Header>
+      <Dimmer.Dimmable>
+        <EnumList enumList={ratList} />
+        <Dimmer active={waitingRatList}>
+          <Loader />
+        </Dimmer>
+      </Dimmer.Dimmable>
       <Header as='h2'>RAN sharing</Header>
+      <Dimmer.Dimmable>
+        <EnumList enumList={ranSharingList} />
+        <Dimmer active={waitingRanSharingList}>
+          <Loader />
+        </Dimmer>
+      </Dimmer.Dimmable>
     </>
   );
 }
