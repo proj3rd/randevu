220cff6b321e43ceff5da4940e24f9ec679f2bbe
Seokseong Jeon <sjeon87@gmail.com>
2021-04-20 13:41:18 +0900

    Support renaming enum

--- front/src/routes/EnumManager.tsx
+++ front/src/routes/EnumManager.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { config} from 'randevu-shared/dist/config';
 import { DocEnum, DocUser } from "randevu-shared/dist/types";
+import { isAdmin, seqValOf } from "randevu-shared/dist/utils";
 import { useEffect, useState } from "react";
 import { Button, Container, Dimmer, Form, Header, Loader, Message, Table } from "semantic-ui-react";
 
@@ -21,6 +22,8 @@ export default function EnumManager({ title, path, user, onLogout }: Props) {
   const [messageVisible, setMessageVisible] = useState(false);
   const [messageContent, setMessageContent] = useState('');
   const [enumList, setEnumList] = useState<DocEnum[]>([]);
+  const [editing, setEditing] = useState<string | undefined>(undefined);
+  const [nameNew, setNameNew] = useState('');
 
   function addEnum() {
     if (!enumName) {
@@ -59,6 +62,35 @@ export default function EnumManager({ title, path, user, onLogout }: Props) {
     });
   }, [path, onLogout]);
 
+  function onClickEdit(_id: string) {
+    const enumFound = enumList.find((enumItem) => enumItem._id === _id);
+    if (!enumFound) {
+      return;
+    }
+    setNameNew(enumFound.name);
+    setEditing(_id);
+  }
+
+  function renameEnum() {
+    if (!editing) {
+      return;
+    }
+    setWaiting(true);
+    axios.post(`${path}/${seqValOf(editing)}`, {
+      name: nameNew,
+    }).then((response) => {
+      return axios.get(path);
+    }).then((response) => {
+      const { data: enumList } = response;
+      setEnumList(enumList);
+    }).catch((reason) => {
+      // TODO: What to do?
+    }).finally(() => {
+      setEditing(undefined);
+      setWaiting(false);
+    })
+  }
+
   return (
     <Container>
       <Header as='h1'>{title}</Header>
@@ -85,18 +117,45 @@ export default function EnumManager({ title, path, user, onLogout }: Props) {
                       ) : (<></>)
                     }
                   </Table.Cell>
-                  <Table.Cell>
+                  <Table.Cell collapsing>
                     <Button type='submit' icon='plus' disabled={!enumName} form='addEnum' />
                   </Table.Cell>
                 </Table.Row>
               ) : (<></>)
             }
             {
-              enumList.map((enumItem) => (
-                <Table.Row key={enumItem._id}>
-                  <Table.Cell colSpan={2}>{enumItem.name}</Table.Cell>
-                </Table.Row>
-              ))
+              enumList.map((enumItem) => {
+                const { _id, name } = enumItem;
+                return (
+                  <Table.Row key={_id}>
+                    <Table.Cell>
+                      {
+                        editing === _id ? (
+                          <>
+                            <Form id='renameEnum' onSubmit={() => renameEnum()}>
+                              <Form.Field>
+                                <input value={nameNew} onChange={(e) => setNameNew(e.target.value)} />
+                              </Form.Field>
+                            </Form>
+                          </>
+                        ) : (<>{name}</>)
+                      }
+                    </Table.Cell>
+                    <Table.Cell collapsing>
+                      {
+                        editing === _id ? (
+                          <>
+                            <Button icon='check' onClick={() => renameEnum()} />
+                            <Button icon='cancel' onClick={() => setEditing(undefined)} />
+                          </>
+                        ) : isAdmin(user) ? (
+                          <Button icon='edit' onClick={() => onClickEdit(_id)} />
+                        ) : (<></>)
+                      }
+                    </Table.Cell>
+                  </Table.Row>
+                )
+              })
             }
           </Table.Body>
         </Table>
