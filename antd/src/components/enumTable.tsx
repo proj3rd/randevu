f3be772fc88fb662ccf860a574427f4bcb2ef6ae
Seokseong Jeon <sjeon87@gmail.com>
2021-05-05 20:00:01 +0900

    Shorten tag

--- antd/src/components/enumTable.tsx
+++ antd/src/components/enumTable.tsx
@@ -120,7 +120,7 @@ export default function EnumTable({ enumList, user, onChangeEnum }: Props) {
           },
         }}
         pagination={false}
-      ></Table>
+      />
     </Form>
   );
 }
