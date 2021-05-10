afd1162753a8e68544f74c7d19e058b572989640
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-05-10 16:08:21 +0900

    Working on /packages

--- antd/src/routes/packages.tsx
+++ antd/src/routes/packages.tsx
@@ -1,3 +1,4 @@
+import { Button, Table } from 'antd/lib';
 import Title from "antd/lib/typography/Title";
 import { DocUser } from "randevu-shared/dist/types";
 
@@ -6,9 +7,16 @@ type Props = {
 };
 
 export default function Packages({ user }: Props) {
+  const columns = [
+    { key: 'name', dataIndex: 'name', title: 'Name' },
+    { key: 'operator', dataIndex: 'operator', title: 'Operator' },
+  ];
+
   return (
     <>
       <Title level={3}>Packages</Title>
+      <Button>Create a package</Button>
+      <Table columns={columns}></Table>
     </>
   )
 }
