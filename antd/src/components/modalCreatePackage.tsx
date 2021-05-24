dffe589063a6c5b07269152f58fa26b558bfb8c7
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-05-25 00:20:43 +0900

    Working on ModalCreatePackage

--- antd/src/components/modalCreatePackage.tsx
+++ antd/src/components/modalCreatePackage.tsx
@@ -1,5 +1,7 @@
 import { Collapse, Form, Input, Modal, Radio, RadioChangeEvent, Select } from "antd";
 import { ModalProps } from 'antd/lib/modal';
+import axios from "axios";
+import { DocEnum, DocOperator, DocPackage } from "randevu-shared/dist/types";
 import { useState } from "react";
 
 type Props = {
@@ -13,19 +15,65 @@ const layout = {
 };
 
 export default function ModalCreatePackage({ ...modalProps }: Props) {
+  const [operatorList, setOperatorList] = useState<DocOperator[]>([]);
+  const [packageMainList, setPackageMainList] = useState<DocPackage[]>([]);
+  const [deploymentOptionList, setDeploymentOptionList] = useState<DocEnum[]>([]);
+  const [productList, setProductList] = useState<DocEnum[]>([]);
+  const [ranSharingList, setRanSharingList] = useState<DocEnum[]>([]);
+
   const [packageType, setPackageType] = useState('main');
 
   function onChangePackageType(e: RadioChangeEvent) {
-    setPackageType(e.target.value ?? 'main');
+    const packageType = e.target.value ?? 'main';
+    setPackageType(packageType);
+    if (packageType === 'sub') {
+      if (!operatorList.length) {
+        axios.get('/operators').then((response) => {
+          const operatorList = response.data as DocOperator[];
+          setOperatorList(operatorList);
+        }).catch((reason) => {
+          console.error(reason);
+        });
+      }
+      if (!packageMainList.length) {
+        // TODO
+      }
+      if (!deploymentOptionList.length) {
+        axios.get('/deployment-options').then((response) => {
+          const deploymentOptionList = response.data as DocEnum[];
+          setDeploymentOptionList(deploymentOptionList);
+        }).catch((reason) => {
+          console.error(reason);
+        });
+      }
+      if (!productList.length) {
+        axios.get('/products').then((response) => {
+          const productList = response.data as DocEnum[];
+          setProductList(productList);
+        }).catch((reason) => {
+          console.error(reason);
+        });
+      }
+      if (!ranSharingList.length) {
+        axios.get('/ran-sharing').then((response) => {
+          const ranSharingList = response.data as DocEnum[];
+          setRanSharingList(ranSharingList);
+        }).catch((reason) => {
+          console.error(reason);
+        });
+      }
+    }
   }
 
   return (
     <Modal
       {...modalProps}
       title='Create a package'
-      forceRender={true}
     >
       <Form
+        initialValues={{
+          packageType: 'main',
+        }}
         {...layout}
       >
         <Form.Item
@@ -37,9 +85,9 @@ export default function ModalCreatePackage({ ...modalProps }: Props) {
         </Form.Item>
         <Form.Item
           label='Package type'
-          name='type'
+          name='packageType'
         >
-          <Radio.Group name='packageType' defaultValue={packageType} onChange={onChangePackageType}>
+          <Radio.Group value={packageType} onChange={onChangePackageType}>
             <Radio value='main'>Main</Radio>
             <Radio value='sub'>Sub</Radio>
           </Radio.Group>
@@ -50,7 +98,16 @@ export default function ModalCreatePackage({ ...modalProps }: Props) {
               label='Operator'
               name='operator'
             >
-              <Select />
+              <Select>
+                {
+                  operatorList.map((operator) => {
+                    const { _id, name } = operator;
+                    return (
+                      <Select.Option key={_id} value={_id}>{name}</Select.Option>
+                    )
+                  })
+                }
+              </Select>
             </Form.Item>
             <Form.Item
               label='Main package'
@@ -74,19 +131,55 @@ export default function ModalCreatePackage({ ...modalProps }: Props) {
               label='Deployment options'
               name='deploymentOptionList'
             >
-              <Select />
+              <Select
+                mode='multiple'
+                allowClear
+              >
+                {
+                  deploymentOptionList.map((deploymentOption) => {
+                    const { _id, name } = deploymentOption;
+                    return (
+                      <Select.Option key={_id} value={_id}>{name}</Select.Option>
+                    )
+                  })
+                }
+              </Select>
             </Form.Item>
             <Form.Item
               label='Products'
               name='productList'
             >
-              <Select />
+              <Select
+                mode='multiple'
+                allowClear
+              >
+                {
+                  productList.map((product) => {
+                    const { _id, name } = product;
+                    return (
+                      <Select.Option key={_id} value={_id}>{name}</Select.Option>
+                    )
+                  })
+                }
+              </Select>
             </Form.Item>
             <Form.Item
               label='RAN sharing'
               name='ranSharingList'
             >
-              <Select />
+              <Select
+                mode='multiple'
+                allowClear
+              >
+                {
+                  ranSharingList.map((ranSharing) => {
+                    const { _id, name } = ranSharing;
+                    return (
+                      <Select.Option key={_id} value={_id}>{name}</Select.Option>
+                    )
+                  })
+                }
+              </Select>
             </Form.Item>
           </Collapse.Panel>
         </Collapse>
