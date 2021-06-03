a6dd3e2835523fe366ad89dd39a50d1850efc6bf
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-06-03 21:51:57 +0900

    Authenticate in /regions

--- antd/src/routes/regions.tsx
+++ antd/src/routes/regions.tsx
@@ -1,5 +1,8 @@
 import Title from "antd/lib/typography/Title";
+import axios from "axios";
 import { DocUser } from "randevu-shared/dist/types";
+import { useEffect } from "react";
+import { useHistory, useRouteMatch } from "react-router";
 
 type Props = {
   user?: DocUser | undefined;
@@ -8,6 +11,22 @@ type Props = {
 };
 
 export default function Regions({ user, setUser, setWaiting: setWaitingApp }: Props) {
+  const history = useHistory();
+  const { url } = useRouteMatch();
+
+  useEffect(() => {
+    setWaitingApp?.(true);
+    axios.get('/authenticate').then((response) => {
+      const { data: user } = response;
+      setUser?.(user);
+    }).catch((reason) => {
+      setUser?.(undefined);
+      history.push(`/login?redirect=${url}`);
+    }).finally(() => {
+      setWaitingApp?.(false);
+    })
+  }, [history, url, setUser, setWaitingApp]);
+
   return (
     <>
       <Title level={3}>Regions</Title>
