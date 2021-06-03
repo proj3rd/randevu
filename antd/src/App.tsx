f7d7feea19e799a6785f55fba6c2d3471e409cdd
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-06-03 21:24:56 +0900

    Add shorthand for passing props

--- antd/src/App.tsx
+++ antd/src/App.tsx
@@ -49,6 +49,9 @@ function App() {
     })
   }
 
+  const setStateActionsToChild = { setUser, setWaiting };
+  const propsToChild = { user, ...setStateActionsToChild };
+
   return (
     <div className="App">
       <Spin spinning={waiting}>
@@ -62,17 +65,17 @@ function App() {
             <Route exact path='/'>
               Landing
             </Route>
-            <Route path='/operators' render={() => <Operators user={user} />} />
-            <Route path='/packages' render={() => <Packages user={user} />} />
-            <Route path='/deployment-options' render={() => <EnumManager title='Deployment options' path='/deployment-options' user={user} />} />
-            <Route path='/duplex-modes' render={() => <EnumManager title='Duplex modes' path='/duplex-modes' user={user} />} />
-            <Route path='/network-elements' render={() => <EnumManager title='Network elements' path='/network-elements' user={user} />} />
-            <Route path='/products' render={() => <EnumManager title='Products' path='/products' user={user} />} />
-            <Route path='/radio-access-technologies' render={() => <EnumManager title='Radio Access Technologies' path='/radio-access-technologies' user={user} />} />
-            <Route path='/ran-sharing' render={() => <EnumManager title='RAN sharing' path='/ran-sharing' user={user} />} />
-            <Route path='/regions' render={() => <Regions user={user} />} />
-            <Route path='/join' render={() => <Join setWaiting={setWaiting} setUser={setUser} />} />
-            <Route path='/login' render={() => <Login setWaiting={setWaiting} setUser={setUser} />} />
+            <Route path='/operators' render={() => <Operators {...propsToChild} />} />
+            <Route path='/packages' render={() => <Packages {...propsToChild} />} />
+            <Route path='/deployment-options' render={() => <EnumManager title='Deployment options' path='/deployment-options' {...propsToChild} />} />
+            <Route path='/duplex-modes' render={() => <EnumManager title='Duplex modes' path='/duplex-modes' {...propsToChild} />} />
+            <Route path='/network-elements' render={() => <EnumManager title='Network elements' path='/network-elements' {...propsToChild} />} />
+            <Route path='/products' render={() => <EnumManager title='Products' path='/products' {...propsToChild} />} />
+            <Route path='/radio-access-technologies' render={() => <EnumManager title='Radio Access Technologies' path='/radio-access-technologies' {...propsToChild} />} />
+            <Route path='/ran-sharing' render={() => <EnumManager title='RAN sharing' path='/ran-sharing' {...propsToChild} />} />
+            <Route path='/regions' render={() => <Regions {...propsToChild} />} />
+            <Route path='/join' render={() => <Join {...setStateActionsToChild} />} />
+            <Route path='/login' render={() => <Login {...setStateActionsToChild} />} />
           </Switch>
         </Content>
       </Spin>
