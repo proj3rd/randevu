70910fc5933129ecba56810fafa5453ae72b14e1
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-06-06 18:50:17 +0900

    Working on OperatorInfo

--- antd/src/routes/operatorInfo.tsx
+++ antd/src/routes/operatorInfo.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, Skeleton } from "antd";
+import { Breadcrumb, Col, Row, Skeleton } from "antd";
 import Title from "antd/lib/typography/Title";
 import axios from "axios";
 import { DocOperator, DocUser } from "randevu-shared/dist/types";
@@ -13,11 +13,18 @@ export default function OperatorInfo({ user }: Props) {
   const { seqVal } = useParams<{ seqVal: string }>();
 
   const [name, setName] = useState('');
+  const [packageList, setPackageList] = useState<DocOperator[]>([]);
 
   useEffect(() => {
     axios.get(`/operators/${seqVal}`).then((response) => {
       const { _id, name } = response.data as DocOperator;
       setName(name);
+    }).catch((reason) => {
+      console.error(reason);
+    });
+    axios.get(`/operators/${seqVal}/packages`).then((response) => {
+      const { data: packageList } = response;
+      setPackageList(packageList);
     }).catch((reason) => {
       console.error(reason);
     });
@@ -34,6 +41,12 @@ export default function OperatorInfo({ user }: Props) {
         }
       </Title>
       <Title level={4}>Frequency allocations</Title>
+      <>
+        <Row>
+          <Col span={6}>Tree</Col>
+          <Col span={18}>Frequency allocations</Col>
+        </Row>
+      </>
       <Title level={4}>Packages</Title>
     </>
   );
